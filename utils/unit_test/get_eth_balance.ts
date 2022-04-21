435d2b9df8f10bd4e1fca477cd60e3d4fb9b6272
Abhijit Roy <dalvath3700@gmail.com>
2022-04-22 02:01:59 +0530

    get eth balance added

---
+++ utils/unit_test/get_eth_balance.ts
@@ -0,0 +1,11 @@
+// import {require} from "@types/node";
+
+const ethers = require('ethers')
+const network = 'rinkeby' // use rinkeby testnet
+const provider = ethers.getDefaultProvider(network)
+const address = '0xF02c1c8e6114b1Dbe8937a39260b5b0a374432bB'
+provider.getBalance(address).then((balance) => {
+    // convert a currency unit from wei to ether
+    const balanceInEth = ethers.utils.formatEther(balance)
+    console.log(`balance: ${balanceInEth} ETH`)
+})
