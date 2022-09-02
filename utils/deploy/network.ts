a506e5990a5f1b3e4681f8518b62cb1b7cb9bf09
default <dalvath3700@gmail.com>
2022-09-02 12:58:24 +0530

    network util file updated

--- utils/deploy/network.ts
+++ utils/deploy/network.ts
@@ -6,15 +6,21 @@ import { ethers } from "hardhat";
 
 // import "@typechain/hardhat"; has to be present in 'hardhat.config.ts' file
 
-const network = await ethers.getDefaultProvider().getNetwork();
+// const network = await ethers.getDefaultProvider().getNetwork();   // always return homestead & chainId: 1
+const network = await ethers.provider.getNetwork();
 const chainId: number = network.chainId;
 console.log(`Network name: ${network.name}`);
 console.log(`Network chain id: ${chainId}`);
 
 // use for running a logic inside the deploy script like this:
 
-if (chainId === 1) {
+if (chainId === 31337) {
   // do something
 } else {
   // do something
 }
+
+// ---------------------
+/* 
+  Metamask assumes localhost:8545 as chain id: 1337, but actually with hardhat, it gives chain id as 31337
+*/
